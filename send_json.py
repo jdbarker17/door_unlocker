ceb13bac6027c8820d7d77f5413759e9b929fa4b
Jon Barker <jdbarker17@gmail.com>
2023-10-09 21:03:36 -0400

    full system integration test

---
+++ send_json.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+url = 'http://0.0.0.0:1717'
+#port = 1717
+headers = {"Content-Type":"application/json"}
+data ={
+    "door_position":"1"
+}
+
+response = requests.post(url,headers = headers, data = json.dumps(data))
+print(response.text)
